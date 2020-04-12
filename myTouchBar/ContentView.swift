64916fa25cd660ff136c4dc74684270dde325b3a
Anton Ovinnikov <anton.ovinnikov@yola.com>
2020-04-12 19:21:30 +0300

    Initial Commit

---
+++ myTouchBar/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  myTouchBar
+//
+//  Created by ao on 12.04.2020.
+//  Copyright © 2020 ao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
